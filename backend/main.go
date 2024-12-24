5ea73e9c8384605ef3080284dde87df5656efb0c
jijimama <ninisesekoma@gmail.com>
2024-12-24 09:12:08 +0900

    DBとの接続とテーブル生成

--- backend/main.go
+++ backend/main.go
@@ -2,16 +2,49 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"context"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+	"github.com/jijimama/newspaper-app/ent"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-  router := gin.Default()
-	
-  router.GET("/", func(c *gin.Context) {
-	c.JSON(200, gin.H{
-      "message": "Hello, World!",
-	})
-  })
+	router := gin.Default()
 
-  router.Run(":8080")
+	// .envファイルを読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// 環境変数から取得
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbName := os.Getenv("DB_NAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	// 接続文字列を作成
+	dsn := "host=" + dbHost + " port=" + dbPort + " user=" + dbUser + " dbname=" + dbName + " password=" + dbPassword + " sslmode=disable"
+
+    //PostgreSQLに接続
+	client, err := ent.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	
+  	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+      		"message": "Hello, World!",
+		})
+  	})
+
+  	router.Run(":8080")
 }
